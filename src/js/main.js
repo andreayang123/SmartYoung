0c9227b411c73c5a1b5d44fe59c5110c656bc8f7
smallkure <smallkure@163.com>
2018-01-07 23:33:59 +0800

    JS ifelse语句

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+var tempArr = [];
+
+tempArr[0] = "jack";
+tempArr[1] = "rose";
+tempArr[2] = "jerry";
+tempArr[3] = "mimi";
+
+
+var tempArr1 = [];
+
+tempArr1[0] = "jack";
+tempArr1[1] = "rose";
+tempArr1[2] = "jerry";
+tempArr1[3] = "mimi";
+
+
+//var name = tempArr[1];
+//push方法用来在数组末尾添加新的元素返回值是新数组的length
+//var tempValue = tempArr.push("ludash");
+//pop方法用来在数组末尾删除一个元素并返回该元素
+//tempArr.pop()
+
+//shift方法用来删除数组首位元素并返回首位元素
+//alert(tempArr.shift());
+//unshift方法用来在数组首位添加一个或者多个元素，返回值是新数组的length
+//alert(tempArr.unshift("tom"));
+//splice方法用来添加或者删除数组中任意位置的元素，有三个参数：①指定索引位置，也可以为负值，从末尾开始，-1就是倒数第一，②指定删除元素的个数，如果为0时不删除任何元素
+//③添加的元素
+//var n1 = tempArr.splice(-2,0,"tom");
+//var n2 = tempArr.splice(1,1,"sb");
+//slice方法用来返回选定的元素，有两个参数，第一个参数指定开始的位置，第二个参数指定结束的位置（不包括结束位置的元素）
+//var newArr = tempArr.slice(-2,-1);
+//concat方法用来连接数组，不会改变原始数组，并返回一个新数组
+//var newArr = tempArr.concat(tempArr1);
+//join方法将数组转换成字符串，并用指定的分隔符连接
+//var newStr = tempArr.join(":");
+//reverse方法用来倒序数组元素的位置
+//var newArr = tempArr.reverse();
+//alert("返回值是:"+ tempArr.toString());
+//sort方法用来对数组元素进行排序，默认排序是从小到大排序，也可以自定义，通过参数方法来进行操作
+//tempArr.sort();
+
